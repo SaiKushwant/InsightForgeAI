77e627dab503a30c9da317679687541f2f2dd21c
SaiKushwant <151735795+SaiKushwant@users.noreply.github.com>
2026-07-06 17:41:28 +0530

    Delete rag directory

--- rag/embeddings.py
+++
@@ -1,9 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-embedding = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5"
-)
-
-vector = embedding.embed_query("What is Retrieval Augmented Generation?")
-
-print("Embedding Dimension:", len(vector))
